804ac8397d968f9d24137ef0aaa2b09b5c62c17f
Hannah Y <hannah@corgibyte.com>
2021-10-19 10:25:10 -0700

    Add dark and light mode buttons.
    Co-authored by: Mike Watkins <michaelhugheswatkins@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -12,4 +12,18 @@ $(document).ready(function() {
     $("#clickable-speed").click(function() {
       $("#answer-text-speed").fadeToggle();    
     });
+
+    $(".to-dark").click(function() {
+      $(".toggle-button").removeClass("to-dark");
+      $(".toggle-button").addClass("to-light");
+      $("body").removeClass();
+      $("body").addClass("dark");
+    });
+
+    $(".to-light").click(function() {
+      $(".toggle-button").removeClass("to-light");
+      $(".toggle-button").addClass("to-dark");
+      $("body").removeClass();
+      $("body").addClass("light");
+    });
 });
